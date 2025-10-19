7cfff5d7043496459b290e605d522f50d16d3450
Dimitri Ntempos <ntemposd@gmail.com>
2025-10-20 01:17:18 +0300

    Revert "Move site from myastro (serve at root)"
    This reverts commit 2de47e7d7dd63702dd07b96e56491a2e8691206e.

---
+++ _posts/2021-05-20-first-post.md
@@ -0,0 +1,12 @@
+---
+layout: post
+title: Baby weeks
+subtitle: Each post also has a subtitle
+image: /img/hello_world.jpeg
+gh-repo: ntemposd/babyweeks
+gh-badge: [star, fork, follow]
+tags: [PyScript, Python]
+comments: true
+---
+
+Lorem Ipsum Dolor Sit Amet
--- astro.config.mjs
+++
@@ -1,9 +0,0 @@
-# myastro/astro.config.mjs
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-export default defineConfig({
-  site: 'https://ntemposd.github.io',
-  base: '/',            // root
-  output: 'static',
-  integrations: [tailwind()],
-})
